2ab7fa5975fda35eee18cc2167482d037a5bee42
HerbertNtim <herbertntim15@gmail.com>
2024-07-26 00:38:37 +0000

    Initial commit

---
+++ src/main/java/com/ntim/domain/USER_ROLE.java
@@ -0,0 +1,6 @@
+package com.ntim.domain;
+
+public enum USER_ROLE {
+    ROLE_ADMIN,
+    ROLE_CUSTOMER
+}
---
+++ src/main/java/com/ntim/domain/VerificationType.java
@@ -0,0 +1,6 @@
+package com.ntim.domain;
+
+public enum VerificationType {
+    MOBILE,
+    EMAIL
+}
