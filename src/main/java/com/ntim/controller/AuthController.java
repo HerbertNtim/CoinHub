614ba04e4cfb29cb23bf7da6abe056139ba5c141
HerbertNtim <herbertntim15@gmail.com>
2024-07-27 00:05:11 +0000

    Login user done

--- src/main/java/com/ntim/controller/AuthController.java
+++ src/main/java/com/ntim/controller/AuthController.java
@@ -7,9 +7,12 @@ import com.ntim.response.AuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> register(@RequestBody User user) throws Exception {
@@ -53,5 +59,38 @@ public class AuthController {
         res.setMessage("Registered successfully");
 
         return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> login(@RequestBody User user) {
+
+        String userEmail = user.getEmail();
+        String userPassword = user.getPassword();
+
+        Authentication auth = authenticate(userEmail, userPassword);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("Logged in successfully");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    private Authentication authenticate(String userEmail, String userPassword) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+        if(userDetails == null){
+            throw new BadCredentialsException("Invalid Email");
+        }
+
+        if(!userPassword.equals(userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid Password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, userPassword, userDetails.getAuthorities());
     }
 }
